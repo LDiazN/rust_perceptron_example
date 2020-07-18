354eb2face91d089d6532de389229a128fcc6c4d
Luis Diaz <ldiazn98@gmail.com>
2020-07-17 22:56:58 -0400

    some testing

--- perceptron/src/main.rs
+++ perceptron/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let usage = "Usage: 
         perceptron filename [options]
         where options:
-            -l arg: Specify a learning 'arg'rate in the range [0,1]
+            -l arg: Specify a learning 'arg' rate in the range [0,1]
             -e arg: Specify a number of epochs 'arg' in the range [0,255] to train
     ";
     
@@ -25,6 +25,10 @@ fn main() {
             Ok(_) => { }
         }
     }
-    
+    //let mut m: perceptron::util::Matrix = ndarray::Array::zeros((10,10));
+    //let v : perceptron::util::NVector = ndarray::Array::ones(10);
+    //let mut col = m.column_mut(0);
+    //
+    //col.iter_mut().enumerate().map(| (i,x) | *x = *x + v[i]);
 
 }
